87e5936a5929ad3d668c3b044a227b3cee8f32a3
Keegan Shudy <shudyk@aldenv189.allegheny.edu>
2016-05-02 13:46:01 -0400

    adding in an additional option to allow users to only download the FASTA file associated with the disease

--- DiseaseChecker.py
+++ DiseaseChecker.py
@@ -10,16 +10,22 @@ import argparse
 import sys
 
 def main(argv):
-	
+
+	#begin argument parsing options
 	parser = argparse.ArgumentParser(description='Analyze disease likelyhood in human DNA.')
 	parser.add_argument('--clean', dest='clean', action='store_true', help="Clean the HTML output file for new diseases")
 	parser.add_argument('--umls', dest='umls', action='store_true', help='Specify if searching using UMLS code')
+	parser.add_argument('--disease-only', dest='diseaseOnly', default=False, action='store_true', help='Specify if wanting to only download the disease clean FASTA file, This option does no comparison')
 
 	parser.set_defaults(clean=False)
 	parser.set_defaults(umls=False)
 	args = parser.parse_args()
 	clean = args.clean
 
+	disease_only = args.diseaseOnly
+
+
+	#Option for cleaning up the HTML file for a new experience
 	if clean:
 		os.remove('HTML/clean.html')
 		copyfile('HTML/doNotDelete.html', 'HTML/clean.html')
@@ -36,14 +42,22 @@ def main(argv):
 	uniprot = UniprotUtil(search_disease, umls=umls)
 	align = Alignment()
 
+	#check if the disease FASTA file already exist
 	if not os.path.isfile('FASTA/%s.fasta' % diseaseU.getName()):
 		uniprot.generateFasta()
+
+	#break if the user only wants to download disease FASTA file
+	if disease_only:
+		print 'Disease file downloaded, location is the FASTA/ directory'
+		return
 
 	likelyhood = diseaseU.getScore()
 
 	clean,dirty = getFastaFiles(diseaseU.getName())
 
+	#case where program is unable to locate a file in the YOU/ directory to compare
 	if clean == None or dirty == None:
+		print "Unable to locate file in YOU/ directory ending..."
 		return
 
 	align_score = align.calculateMutationPercentage(clean,dirty)
@@ -54,7 +68,7 @@ def main(argv):
 	writer.writeTableOutput(diseaseU.getName(), str(round(likelyhood*align_score,2)) + '%')
 	webbrowser.open('HTML/index.html', new=2)
 
-
+#gets the two sequences from the FASTA folder (seq1) and the YOU folder (seq2)
 def getFastaFiles(diseaseName):
 	seq1 = open('FASTA/%s.fasta' % diseaseName)
 	header = seq1.readline()
@@ -68,7 +82,6 @@ def getFastaFiles(diseaseName):
 	else:
 		file = findFastaFile(header.split('|'))
 		if file == None:
-			print "Unable to locate file in YOU/ directory ending..."
 			return [None,None]
 		file.readline()
 		seq2 = file.read().replace('\n','')
